7d47d054da81bf670430a8ff98764952c74c4b6c
cwiertniamichal <cwiertnia.michal@gmail.com>
2023-07-03 22:23:33 +0200

    Init

---
+++ src/test/java/cwiertnia/michal/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package cwiertnia.michal.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
