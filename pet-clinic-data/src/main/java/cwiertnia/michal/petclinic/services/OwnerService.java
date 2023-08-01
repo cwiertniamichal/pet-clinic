aba7903ee38adf68dac6aa92bb5c6eb39de53d3b
cwiertniamichal <cwiertnia.michal@gmail.com>
2023-08-01 20:54:51 +0200

    Services refactor

--- pet-clinic-data/src/main/java/cwiertnia/michal/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/cwiertnia/michal/petclinic/services/OwnerService.java
@@ -2,12 +2,6 @@ package cwiertnia.michal.petclinic.services;
 
 import cwiertnia.michal.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/cwiertnia/michal/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/cwiertnia/michal/petclinic/services/VetService.java
@@ -1,12 +1,7 @@
 package cwiertnia.michal.petclinic.services;
 
-import cwiertnia.michal.petclinic.model.Owner;
 import cwiertnia.michal.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
