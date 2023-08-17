8eea9de9fbd56b6b0174f67374ff2e1a379e4729
cwiertniamichal <cwiertnia.michal@gmail.com>
2023-08-17 20:04:08 +0200

    Autogenerated IDs in Map impl. Closes #21.

--- pet-clinic-data/src/main/java/cwiertnia/michal/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/cwiertnia/michal/petclinic/services/map/OwnerServiceMap.java
@@ -8,11 +8,6 @@ import org.springframework.stereotype.Service;
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
-    public Owner save(Owner owner) {
-        return save(owner.getId(), owner);
-    }
-
-    @Override
     public Owner findByLastName(String lastName) {
         return null;
     }
