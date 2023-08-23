148f69c3ecc9f85e03797fa11b16d55a2a54a5ee
cwiertniamichal <cwiertnia.michal@gmail.com>
2023-08-23 22:58:19 +0200

    Fixed menu links. Closes #41.

--- pet-clinic-web/src/main/java/cwiertnia/michal/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/cwiertnia/michal/petclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package cwiertnia.michal.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +9,10 @@ public class IndexController {
     @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oups(Model model) {
+        return "not_implemented";
     }
 }
