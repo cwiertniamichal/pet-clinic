f7265d631c2c08b7df21d6126c9ace669740da30
cwiertniamichal <cwiertnia.michal@gmail.com>
2023-08-23 21:45:38 +0200

    Added pet type to bootstrap. Closes #28

--- pet-clinic-web/src/main/java/cwiertnia/michal/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/cwiertnia/michal/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package cwiertnia.michal.petclinic.bootstrap;
 
 import cwiertnia.michal.petclinic.model.Owner;
+import cwiertnia.michal.petclinic.model.PetType;
 import cwiertnia.michal.petclinic.model.Vet;
 import cwiertnia.michal.petclinic.services.OwnerService;
+import cwiertnia.michal.petclinic.services.PetTypeService;
 import cwiertnia.michal.petclinic.services.VetService;
 import cwiertnia.michal.petclinic.services.map.OwnerServiceMap;
 import cwiertnia.michal.petclinic.services.map.VetServiceMap;
@@ -14,14 +16,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
