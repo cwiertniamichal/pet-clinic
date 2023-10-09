0a4c96c41f1758854e39e25c715723cdfcee9aec
cwiertniamichal <cwiertnia.michal@gmail.com>
2023-10-09 21:34:20 +0200

    Added JPA service for speciality. Closes #40.

---
+++ pet-clinic-data/src/main/java/cwiertnia/michal/petclinic/services/jpa/SpecialityServiceJpa.java
@@ -0,0 +1,48 @@
+package cwiertnia.michal.petclinic.services.jpa;
+
+import cwiertnia.michal.petclinic.model.Speciality;
+import cwiertnia.michal.petclinic.repositories.SpecialityRepository;
+import cwiertnia.michal.petclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class SpecialityServiceJpa implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityServiceJpa(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialityRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
